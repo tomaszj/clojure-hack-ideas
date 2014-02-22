5634341d5c8e7a4b5c4c1f92c50f0903d874e48d
Tomasz Janeczko <janecztom@gmail.com>
2014-02-22 17:54:45 +0000

    Added simple framework for rendering pages, added Bootstrap to the project.

--- src/hack_ideas/handler.clj
+++ src/hack_ideas/handler.clj
@@ -5,23 +5,55 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
-(defn render-page "Renders a basic HTML template" []
-  (html5 
+(defn hack-ideas-path [] "/hack-ideas"
+  )
+
+(defn render-navbar "Renders a navigation bar" []
+  [:nav.navbar.navbar-inverse {:role "navigation"}
+   [:div.navbar-header [:a.navbar-brand {:href "#"} "Hack Ideas!"]]
+   [:ul.nav.navbar-nav 
+      [:li [:a {:href (hack-ideas-path)} "Explore ideas"]]
+    ] 
+   ]
+  )
+
+(defn render-page "Renders a basic HTML template" [content]
+  (html5 {:lang "en"}
     [:head
       [:title "Hack Ideas"]
-      (include-css "//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css")]
+      [:meta {:charset "utf-8"}]
+      (include-css "/css/bootstrap.min.css")
+      (include-css "/css/bootstrap-theme.min.css")
+      (include-css "/css/main.css")
+      ]
     [:body
-       "Hello world!"
+      [:div.container
+        (render-navbar)
+        [:div.jumbotron 
+         [:h1 "Hack Ideas!"]
+         [:h4 "The best place to create and look for brilliant hack ideas."]
+         ]
+        [:div.main_content content]
+       ]
+      (include-js "/js/jquery/dist/jquery.min.js")
+      (include-js "/js/bootstrap/dist/js/bootstrap.min.js")
      ]
     )
   )
 
 (defn root-handler "Main handler for the page" [request]
-  (render-page)
+  (render-page "")
+  )
+
+(defn get-hack-ideas-handler [request]
+  (render-page "ALL HACK IDEAS HERE!")
   )
 
 (defroutes app-routes
   (GET "/" [] root-handler)
+  (context "/hack-ideas" [] (defroutes ideas-routes 
+    (GET "/" [] get-hack-ideas-handler)                       
+    ))
   (route/resources "/")
   (route/not-found "Not Found"))
 
