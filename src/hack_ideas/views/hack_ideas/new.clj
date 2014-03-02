b6571df89ddfbe4f73dc71e50c255ac9299d9196
Tomasz Janeczko <janecztom@gmail.com>
2014-03-02 01:18:50 +0000

    Changed basic html form inputs to Hiccup.

--- src/hack_ideas/views/hack_ideas/new.clj
+++ src/hack_ideas/views/hack_ideas/new.clj
@@ -1,24 +1,27 @@
 (ns hack-ideas.views.hack-ideas.new
-  (:require [hack-ideas.routes-helper :refer :all])
-  (:require [hiccup.core :refer :all]))
+  (:require [hack-ideas.routes-helper :refer :all]
+            [hiccup.core :refer :all]
+            [hiccup.form :refer :all]))
 
 (defn render []
   (html
     [:h2 "Create a new idea!"]
     [:div.new-idea.col-lg-4
-     [:form {:action (hack-ideas-path) :method "POST"}
-      [:div.form-group 
-       [:label {:for "hack-idea[name]"} "New idea name"]
-       [:input.form-control {:type "text" :id "hack-idea[name]" :name "hack-idea[name]"}]
-       ] 
-      [:div.form-group
-       [:label {:for "hack-idea[description]"} "And its description"]
-       [:textarea.form-control {:rows "3" :id "hack-idea[description]" :name "hack-idea[description]"}]
-       ] 
-      [:div.form-group
-       [:input.form-control.btn.btn-primary {:type "submit" :value "Create it!"}]
-       ] 
-      ]
+     (form-to [:post (hack-ideas-path)] 
+      (with-group "hack-idea"
+        [:div.form-group 
+         (label "name" "New idea name")
+         (text-field {:class "form-control"} "name")
+         ] 
+        [:div.form-group
+         (label "description" "And its description")
+         (text-area {:class "form-control" :rows "3"} "description")
+         ] 
+        [:div.form-group
+         (submit-button {:class "form-control btn btn-primary"} "Create it!")
+         ] 
+        )
+      ) 
      ]
     )
   )
